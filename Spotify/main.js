cd166236a934b6023967ea1aec673aa112393991
sojungnancy <sojungnancy>
2025-01-11 15:36:19 +0900

    update

--- Spotify/main.js
+++ Spotify/main.js
@@ -1,8 +1,6 @@
-// DOM 요소 가져오기
 const searchButton = document.getElementById('searchButton');
 const searchBar = document.getElementById('searchBar');
 
-// 검색 아이콘 클릭 시 상태 토글
 searchButton.addEventListener('click', () => {
   if (searchBar.classList.contains('hidden')) {
     // 검색 바 열기
@@ -12,5 +10,20 @@ searchButton.addEventListener('click', () => {
     // 검색 바 닫기
     searchBar.classList.remove('visible');
     searchBar.classList.add('hidden');
+  }
+});
+
+const signupButton = document.getElementById('signupButton');
+const footerArea = document.getElementById('footerArea');
+
+signupButton.addEventListener('click', () => {
+  if (footerArea.classList.contains('hidden')) {
+    // 검색 바 열기
+    footerArea.classList.remove('hidden');
+    footerArea.classList.add('visible');
+  } else {
+    // 검색 바 닫기
+    footerArea.classList.remove('visible');
+    footerArea.classList.add('hidden');
   }
 });
